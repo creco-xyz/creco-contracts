2e9f7db06a2c472cf1f6d6004c3f0907654dc807
philipplgh <philipplgh@gmail.com>
2022-11-26 14:34:05 +0000

    init

---
+++ hardhat.config.ts
@@ -0,0 +1,16 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 1337, // allow metamask testing
+    },
+  },
+};
+
+export default config;
